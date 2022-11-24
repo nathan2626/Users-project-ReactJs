d9dce65d4718d05e1f0804fe95c5a61ea185b2b9
Natan Journo <natanjourno@MacBook-Pro-de-Natan.local>
2022-11-24 14:45:10 +0100

    Adding validation ❌✅

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -12,6 +12,16 @@ const AddUser = (props) => {
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 ) {
+            console.log('Empty value');
+            return;
+            
+        } else  if(+enteredAge < 1) {
+            console.log('Age cannot be less than 0');
+            return;
+        }
+
         console.log(enteredUsername, enteredAge);
 
         //for reset a input value
